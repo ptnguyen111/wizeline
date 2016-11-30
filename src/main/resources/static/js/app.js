8db7d15801bacff74c1b3137ceffd171547fc9c7
Nguyen Pham <ptnguyen111@gmail.com>
2016-11-30 17:52:10 +0700

    nguyen: html template

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("UrlShortenerApp", []);
 
 app.controller("HomeController", function ($scope, $http) {
-
+    $scope.personalized = false;
 });
 
 app.controller("TestUrlController", function ($scope, $http) {
