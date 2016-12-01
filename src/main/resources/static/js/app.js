b924adc42844a8d4eeb7bf901249ddd52acf081c
Nguyen Pham <ptnguyen111@gmail.com>
2016-12-01 09:15:56 +0700

    nguyen: UI template and angular controller

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,8 +2,34 @@ var app = angular.module("UrlShortenerApp", []);
 
 app.controller("HomeController", function ($scope, $http) {
     $scope.personalized = false;
-});
 
-app.controller("TestUrlController", function ($scope, $http) {
+    $scope.generate = function() {
+        if (!$scope.fullURL) {
+            return;
+        }
+        console.log($scope.customShortUrl);
+        $scope.shorternURL = angular.isDefined($scope.customShortUrl) ? $scope.customShortUrl : "";
 
+
+        $http.post("/rest/url?fullUrl=" + $scope.fullURL + "&customUrl=" + $scope.shorternURL)
+            .then(function(response) {
+                console.log(response);
+                $scope.shorternURL = response.data.url;
+            }, function (error) {
+                console.log(error);
+            })
+
+    };
+
+    $scope.getFull = function() {
+        if (!$scope.fullURL) {
+            return;
+        }
+
+        $http.get("/rest/url?shortUrl=" + $scope.fullURL)
+            .then(function(response) {
+                console.log(response);
+                $scope.shorternURL = response.data.url;
+            });
+    }
 });
